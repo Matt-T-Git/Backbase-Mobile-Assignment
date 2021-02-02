4aebd578cb251bb308651ecd9e7c21f60631beb8
Mathew Thomas <53473163+JoJoMathew@users.noreply.github.com>
2021-02-02 17:55:58 +0000

    set up storyboard

--- City-Search/City-Search/ViewControllers/ListViewController.swift
+++ City-Search/City-Search/ViewControllers/ListViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
